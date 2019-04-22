2c884a98dcf55c5d0828ec1c516949c27fd214e3
Lianjifu <1192259548@qq.com>
2019-04-22 18:08:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 # Application配置参数
 settings = dict(
         static_path=os.path.join(os.path.dirname(__file__), "static"),
-        # cookie_secret="FhLXI+BRRomtuaG47hoXEg3JCdi0BUi8vrpWmoxaoyI=",
+        # cookie_secret="",
         # xsrf_cookies=True,
         debug=True
     )
@@ -13,10 +13,10 @@ settings = dict(
 
 # 数据库配置参数
 mysql_options = dict(
-    host="rm-bp1woxm70a39b9lixo.mysql.rds.aliyuncs.com",
-    database="ywgsh_wxshop_dev",
-    user="ywgsh_wxs_root",
-    password="Wxs2018",
+    host="",
+    database="",
+    user="",
+    password="",
 )
 
 # # Redis配置参数
@@ -30,6 +30,6 @@ log_path = os.path.join(os.path.dirname(__file__), "logs/log")
 log_level = "debug"
 
 # access_token
-token = "dsfdCjaTXQdX2jpupQFQLoQo5N4OkEmkeHsHD9+BBx2WQ="
+token = ""
 
 
